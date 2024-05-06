614f4291a1c976f9654371233e7921815e7f22b6
YawenXXX <109942054+YawenXXX@users.noreply.github.com>
2024-05-05 19:40:21 -0700

    Losses & train

--- Fastmri_unet_dev/Training/unet_train.py
+++ Fastmri_unet_dev/Training/unet_train.py
@@ -1,11 +1,22 @@
 '''
 THis file is used for implement common used functions
 '''
+import os
+
 import torch
+import numpy as np
+from fastmri.models import Unet
 from torch import nn
 from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from Training.losses import ssim_loss
+import Utils
 
-def train_epoch(args, model, train_loader, loss, optimizer):
+import config_file
+import pickle
+
+
+def train_epoch(args, model, train_loader, loss_ssim, optimizer):
     '''
     Training process for one single epoch
     Args:
@@ -15,9 +26,44 @@ def train_epoch(args, model, train_loader, loss, optimizer):
 
     '''
 
+    model.train()
+    total_loss = 0
+
+    for batch in train_loader:
+        inputs, targets = batch[0], batch[1]
+        inputs, targets = inputs.to(args.device), targets.to(args.device)
+
+        # inputs = complex_to_channels(inputs)
+        # print("inputs.shape: ", inputs.shape)
+        # print("targets.shape: ", inputs.shape)
+        inputs = complex_to_magnitude(inputs)
+        targets = complex_to_magnitude(targets)
+        inputs = inputs.float()
+        targets = targets.float()
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        # print(outputs.size())
+        loss = ssim_loss(outputs, targets)
+        loss.to(device=args.device)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(train_loader)
 
 
-def validate(args, model, val_loader):
+def complex_to_magnitude(data):
+    # Ensure the input is complex and has an imaginary part
+    if torch.is_complex(data):
+        magnitude = torch.abs(data)
+    else:
+        magnitude = data  # Assuming data is already real
+    return magnitude
+
+
+def validate(args, model, val_loader, criterion):
     '''
     Validation process
     Args:
@@ -25,8 +71,28 @@ def validate(args, model, val_loader):
         val_loader: validation loader for iterating data
 
     '''
+    model.eval()  # Set the model to evaluation mode
+    total_loss = 0
+    with torch.no_grad():  # Disable gradient computation for efficiency and to prevent model from learning
+        for batch in val_loader:
+            inputs, targets = batch[0], batch[1]
+            inputs, targets = inputs.to(args.device), targets.to(args.device)
 
-def save_model(export_dir, ):
+            # Convert inputs and targets to their magnitudes if they're complex
+            inputs = complex_to_magnitude(inputs)
+            targets = complex_to_magnitude(targets)
+            inputs = inputs.float()
+            targets = targets.float()
+
+            outputs = model(inputs)
+            loss = ssim_loss(outputs, targets)
+
+            total_loss += loss.item()
+
+    return total_loss / len(val_loader)
+
+
+def save_model(epoch, model, export_dir):
     '''
     Save model during training
     Args:
@@ -35,9 +101,20 @@ def save_model(export_dir, ):
     Returns:
 
     '''
+    os.makedirs(export_dir, exist_ok=True)
+
+    file_path = f'{export_dir}/model_epoch_{epoch}.pkl'
+
+    # Open a file to write the pickled data
+    with open(file_path, 'wb') as file:
+        pickle.dump(model, file)
+
+    print(f"Model saved to {file_path}")
+
+    return file_path
 
 
-def train(args, model, loss, optimizer, train_loader, val_loader, num_epochs):
+def train(args, model, loss_ssim, optimizer, train_loader, val_loader):
     '''
     Implementing Training Process
     Args:
@@ -50,12 +127,35 @@ def train(args, model, loss, optimizer, train_loader, val_loader, num_epochs):
 
     '''
     start_epoch = 0
+    export_dir = config_file.OUTPUT_DIR
+    val_loss_log = np.empty((0, 2))
+    val_log_path = config_file.LOG_FILE_PATH
+    train_loss_log = np.empty((0, 2))
+    train_log_path = config_file.LOG_FILE_TRAIN
+    best_val_loss = 1
 
-    for epochs in range(start_epoch, num_epochs):
-        # Run single one epoch to get training loss
-        train_loss,  = train_epoch(args, model, train_loader, loss, optimizer)
-        # validation loss
-        val_loss,  = validate(args, model, val_loader)
+    for epoch in range(start_epoch, args.num_epochs):
+        print(f'Epoch {epoch} ------------start training-------------')
+
+        train_loss = train_epoch(args, model, train_loader, loss_ssim, optimizer)
+        val_loss = validate(args, model, val_loader, loss_ssim)
+
+        print(f'Epoch {epoch}, Train Loss: {train_loss}, Validation Loss: {val_loss}')
+        val_loss_log = np.append(val_loss_log, np.array([[epoch, val_loss]]), axis=0)
+        train_loss_log = np.append(train_loss_log, np.array([[epoch, train_loss]]), axis=0)
+        np.save(val_log_path, val_loss_log)
+        np.save(train_log_path, train_loss_log)
+        print(f'Loss File saved')
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_epoch = (epoch, best_val_loss)
+            # Utils.save_reconstruction(export_dir, reconstructions)
+        save_model(epoch, model, export_dir)
+
+    return val_loss_log, best_epoch
+
+
 
 
 
